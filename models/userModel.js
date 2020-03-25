66b712388f4064f9edf1499da6244491703a9f1a
Xyphnos <arttuva@gmail.com>
2020-03-25 15:09:24 +0200

    Fixed models

--- models/userModel.js
+++ models/userModel.js
@@ -1,41 +1,5 @@
 'use strict';
 const mongoose = require('mongoose');
-const users = [
-    {
-        id: '1',
-        name: 'John Doe',
-        email: 'john@metropolia.fi',
-        password: '1234',
-    },
-    {
-        id: '2',
-        name: 'Jane Doez',
-        email: 'jane@metropolia.fi',
-        password: 'qwer',
-    },
-];
-
-const getUser = (id) => {
-    const user = users.filter((usr) => {
-        if (usr.id === id) {
-            return usr;
-        }
-    });
-    return user[0];
-};
-
-
-const getUserLogin = async (params) => {
-    try {
-        console.log(params);
-        const [rows] = await promisePool.execute(
-            'SELECT * FROM wop_user WHERE email = ?;',
-            params);
-        return rows;
-    } catch (e) {
-        console.log('error', e.message);
-    }
-};
 
 const Schema = mongoose.Schema;
 
@@ -47,8 +11,3 @@ const userSchema = new Schema({
 });
 
 module.exports = mongoose.model('user', userSchema);
-
-module.exports = {
-    users,
-    getUserLogin,
-};
